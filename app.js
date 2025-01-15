8d71431c19ff53414f7fa8e86ab46ed780d1d210
Kevin wasonga <kevinwasonga415@outlook.com>
2025-01-15 05:56:01 +0300

    index

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
+
 app.set('view engine', 'pug');
+app.use(express.static('public'));
+
+// app.use((req, res, next) => {
+//   req.requestTime = new Date().toISOString();
+//   res.setHeader(
+//     "Content-Security-Policy",
+//     "default-src 'none' script-src 'self' data https://apis.google.com 'unsafe-eval' https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"
+//   );
+//   next();
+// });
+
 
 app.get('/', (req, res) => {
     res.render('index', {
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./public/js/index.js",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "public/dist"),
+  },
+  watch: true
+};
