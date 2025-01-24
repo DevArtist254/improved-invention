30ff89ee5e6c372f2daf68a361c9b6f1f4fcbd76
Kevin wasonga <kevinwasonga415@outlook.com>
2025-01-24 05:57:11 +0300

    content-sticky

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,21 @@ import store from "./store/store";
 import search from "./ui/search";
 
 
+const sideBar = document.querySelector(".images__sec");
+const main = document.getElementById("main");
+const sideBarTop = sideBar.offsetTop;
+
+window.addEventListener("scroll", () => {
+  const parentRect = main.getBoundingClientRect();
+  const sidebarRect = sideBar.getBoundingClientRect();
+
+  if (parentRect.top <= 20 && parentRect.bottom - sidebarRect.height >= 20) {
+    sideBar.classList.add("sticky");
+  } else {
+    sideBar.classList.remove("sticky");
+  }
+})
+
 const init = function () {
   search.handleAnimation();
   search.handleSearch();
